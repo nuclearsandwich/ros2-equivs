d980ffbeda53e0a8c9c15460b5e7ecfc5a95d2ef
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-11-23 22:37:00 -0800

    Update package internals for 2.7.

--- gurumdds-2.7-armhf/build.sh
+++ gurumdds-2.7-armhf/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -v$(pwd):/tmp/gurumdds-2.6 osrf/ubuntu_armhf:bionic sh -c 'apt update && \
+docker run -v$(pwd):/tmp/gurumdds-2.7 osrf/ubuntu_armhf:bionic sh -c 'apt update && \
 	apt install -y equivs && \
-	cd /tmp/gurumdds-2.6 && \
-	equivs-build gurumdds-2.6'
+	cd /tmp/gurumdds-2.7 && \
+	equivs-build gurumdds-2.7'
